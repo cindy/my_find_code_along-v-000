3d64a400484dbcc6e8577516b7298348d900f585
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-18 02:09:04 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,14 +3,18 @@ require 'pry'
 def my_find(collection)
   i = 0
   finds = []
-  while i < collection.size
-    #binding.pry
-    finds << collection[i] if yield(collection[i])
-    #binding.pry
-    i += 1
+  if block_given?
+    while i < collection.size
+      #binding.pry
+      finds << collection[i] if yield(collection[i])
+      #binding.pry
+      i += 1
+    end
+  else
+    finds << yield
   end
   finds
 end
 
 
-puts my_find([1,2,3]){ |i| i ==3 }
+#puts my_find([1,2,3]){ |i| i ==3 }
